48b9d78035b9033860354ad0b30ab1fa3014c012
thakkardhavalc <thakkardhavalc@gmail.com>
2024-01-22 11:09:53 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created By dhhaval thakkar on 2024-01-22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/in28minutes/junit/MyMath.java
@@ -0,0 +1,7 @@
+package com.in28minutes.junit;
+
+/**
+ * Created By dhhaval thakkar on 2024-01-22
+ */
+public class MyMath {
+}
