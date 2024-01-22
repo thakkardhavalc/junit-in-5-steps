7e785bc082be8e7fc7d0d96057e139b62eefdff1
thakkardhavalc <thakkardhavalc@gmail.com>
2024-01-22 11:31:37 -0700

    added junit5 library

--- src/com/in28minutes/junit/MyMath.java
+++ src/com/in28minutes/junit/MyMath.java
@@ -4,4 +4,15 @@ package com.in28minutes.junit;
  * Created By dhhaval thakkar on 2024-01-22
  */
 public class MyMath {
+
+    public int calculateSum(int[] numbers) {
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
 }
---
+++ test/com/in28minutes/junit/MyMathTest.java
@@ -0,0 +1,16 @@
+package com.in28minutes.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Created By dhhaval thakkar on 2024-01-22
+ */
+public class MyMathTest {
+
+    @Test
+    void test() {
+
+    }
+}
